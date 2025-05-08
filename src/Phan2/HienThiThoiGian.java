da711e0b300e7ecc9b8336099ba07a1448052268
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-05-08 14:09:51 +0700

    add

---
+++ src/Phan2/HienThiThoiGian.java
@@ -0,0 +1,11 @@
+package Phan2;
+
+import java.util.Date;
+
+public class HienThiThoiGian {
+    public static void main(String[] args) {
+
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
---
+++ src/Phan2/KhaiBaoVaDungBien.java
@@ -0,0 +1,7 @@
+package Phan2;
+
+public class KhaiBaoVaDungBien {
+    public static void main(String[] args) {
+
+    }
+}
